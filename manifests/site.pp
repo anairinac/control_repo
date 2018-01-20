1a78bc96123693574a5691116ddfe59911e5ade4
Ana Irina Calvo Carvajal <anairinac@gmail.com>
2018-01-19 18:43:58 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,3 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a cool readme file',
-    owner   => 'root',
-  }
-  file {'/root/README':
-    owner => 'root',
-  }
+
 }
